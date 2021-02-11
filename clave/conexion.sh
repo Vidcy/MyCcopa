bfb79db710273b0b5b7674e4cc7377cb1422e3b3
vidcy <davidccopayapura@gmail.com>
2021-02-11 15:59:22 -0500

    first commit

---
+++ clave/conexion.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+ssh -i "My_patron_acces.pem" ubuntu@ec2-52-44-152-246.compute-1.amazonaws.com
---
+++ exam2/conexion.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+ssh -i "CCOPA-EXAMN.pem" ubuntu@ec2-34-236-18-154.compute-1.amazonaws.com
